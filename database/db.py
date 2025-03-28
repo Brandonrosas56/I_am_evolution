7b5aa6835e734dcda30473dad6887fadfdd52ea2
Brandonrosas56 <brayanrosas635@gmail.com>
2025-03-28 14:14:10 -0500

    git push --set-upstream origin Login

--- database/db.py
+++ database/db.py
@@ -1,10 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-import os
 
 # URL de conexión a MongoDB
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
-# Nombre de la base de datos
 DATABASE_NAME = "Evolution"
 
 class Database:
